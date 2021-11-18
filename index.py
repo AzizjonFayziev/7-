dbd86ff9cec70f38d8a8636125276d2e535b99c0
Fayziev Azizjon <azizdjon75@gmail.com>
2021-11-18 19:53:56 +0500

    Index.py

--- index.py
+++ index.py
@@ -1,8 +1,35 @@
-# num1 = int(input('Enter 1st number:'))
-# num2 = int(input('Enter 2nd number:'))
-# if num1 > num2: print(f'{num2} - min number')
-# else: print(f'{num1} - min number')
+names = ['Zafar', 'Abdulkhakim', 'Bobur', 'Zakir', 'Shukhrat', 'Aziz', 'Otabek', 'Amalmalik']
+ages = [21, 15, 17, 20]
+# print(names[-1])
+# print(names[-2])
+# print(names[-2])
+# names.append('Elbek')
+# print(names)
 
-num = int(input('Enter number:'))
-if num > 0: print(num ** 2)
-elif num < 0: print(num ** (1 / 3))
+# print(names.pop(0))
+# print(names.pop(0))
+# print(names.pop(0))
+# print(names.pop(0))
+# print(names)
+
+# print(len(names))
+
+# names.remove('Bobur')
+# names.remove(21)
+# print(names)
+
+# ages.remove(21)
+# ages.remove(29)
+
+# print(ages)
+# names.reverse()
+
+# print(names[::-1])
+# print(names)
+
+# names = names[::-1]
+# print(names)
+
+# print(names[0:2])
+
+# print(names[0:7:2])
