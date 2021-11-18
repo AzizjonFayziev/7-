d790114965c2d28ee5b8f8dda0453e13d9049745
Fayziev Azizjon <azizdjon75@gmail.com>
2021-11-18 20:03:47 +0500

    How works with hours

--- index.py
+++ index.py
@@ -33,3 +33,16 @@ ages = [21, 15, 17, 20]
 # print(names[0:2])
 
 # print(names[0:7:2])
+
+# print(names[0::-1])
+
+
+# hrs = int(input('Enter hours:'))
+# mins = int(input('Enter mins:'))
+# delta = int(input('Enter delta hours:'))
+
+# if hrs >= 0 and  0 <= mins <= 59:
+#     new_hrs = (hrs + delta) % 24
+#     if new_hrs <= 9: new_hrs = '0' + str(new_hrs)
+#     if mins <= 9 : mins = '0' + str(mins)
+#     print(f'{new_hrs}:{mins}')
